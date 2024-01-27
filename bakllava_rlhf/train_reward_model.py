4709a936aceb177d696d7805585e16c69c09e431
Fraser Mince <frasermince@gmail.com>
2024-01-27 14:31:12 -0600

    Init project with reward trainer.

---
+++ bakllava_rlhf/train_reward_model.py
@@ -0,0 +1,264 @@
+from transformers import (
+    BitsAndBytesConfig,
+    LlavaForConditionalGeneration,
+    AutoProcessor,
+)
+from dataclasses import dataclass, field
+import torch
+import transformers
+from trl import (
+    RewardConfig,
+    RewardTrainer,
+)
+from typing import Optional, List, Literal
+from datasets import load_dataset
+from peft import LoraConfig
+
+
+@dataclass
+class ModelArguments:
+    model_name_or_path: Optional[str] = field(default="EleutherAI/pythia-12b")
+    trust_remote_code: Optional[bool] = field(
+        default=False,
+        metadata={
+            "help": "Enable unpickling of arbitrary code in AutoModelForCausalLM#from_pretrained."
+        },
+    )
+    # from LLaVA
+    version: Optional[str] = field(default="v1")
+    freeze_backbone: bool = field(default=False)
+    tune_mm_mlp_adapter: bool = field(default=False)
+    vision_tower: Optional[str] = field(default=None)
+    mm_vision_select_layer: Optional[int] = field(
+        default=-1
+    )  # default to the last layer
+    pretrain_mm_mlp_adapter: Optional[str] = field(default=None)
+    mm_use_im_start_end: bool = field(default=False)
+    mm_use_im_patch_token: bool = field(default=True)
+    mm_vision_select_feature: Optional[str] = field(default="patch")
+
+
+@dataclass
+class DataArguments:
+    dataset_path: str = field(default="tatsu-lab/alpaca_farm")
+    dataset_name: str = field(default=None, metadata={"help": "Dataset name"})
+    eval_dataset_path: str = field(default="tatsu-lab/alpaca_farm")
+    eval_dataset_name: str = field(default="alpaca_human_preference")
+    eval_size: int = field(
+        default=500,
+        metadata={
+            "help": "Number of examples to split out from training to use for evaluation."
+        },
+    )
+    # From LLaVA
+    lazy_preprocess: bool = False
+    is_multimodal: bool = False
+    image_folder: Optional[str] = field(default=None)
+    image_aspect_ratio: str = "square"
+    image_grid_pinpoints: Optional[str] = field(default=None)
+    reward_prompt_file: Optional[str] = field(default=None)
+    image_to_caption_file: Optional[str] = field(default=None)
+
+
+@dataclass
+class TrainingArguments(transformers.Seq2SeqTrainingArguments):
+    cache_dir: Optional[str] = field(default=None)
+    # From LLaVA
+    remove_unused_columns: bool = field(default=False)
+    freeze_mm_mlp_adapter: bool = field(default=False)
+    # From AlpacaFarm
+    model_max_length: int = field(
+        default=512,
+        metadata={
+            "help": "Maximum sequence length. Sequences will be left padded to this length always during training."
+        },
+    )
+    query_len: int = field(default=None, metadata={"help": "Length of the query."})
+    response_len: int = field(
+        default=None, metadata={"help": "Length of the response."}
+    )
+    label_names: List[str] = field(
+        default_factory=lambda: ["index_0", "index_1", "choice"],
+        metadata={
+            "help": "Names of the labels in the dataset. "
+            "This is needed to get transformers.Trainer to not throw those tensors away before `compute_loss`."
+            "By default, the trainer throws away columns it doesn't recognize when creating the "
+            "`train_dataloader` (see `_remove_unused_columns`). "
+        },
+    )
+    padding: Literal["max_length", "longest"] = field(
+        default="longest",
+        metadata={
+            "help": "Padding strategy. If 'max_length', pads to `model_max_length` always; this might lead to some "
+            "redundant compute. If 'longest', pads to the longest sequence in the batch, capped by `model_max_length`."
+        },
+    )
+    # From QLoRA
+    full_finetune: bool = field(
+        default=False, metadata={"help": "Finetune the entire model without adapters."}
+    )
+    adam8bit: bool = field(default=False, metadata={"help": "Use 8-bit adam."})
+    double_quant: bool = field(
+        default=True,
+        metadata={
+            "help": "Compress the quantization statistics through double quantization."
+        },
+    )
+    quant_type: str = field(
+        default="nf4",
+        metadata={
+            "help": "Quantization data type to use. Should be one of `fp4` or `nf4`."
+        },
+    )
+    bits: int = field(default=4, metadata={"help": "How many bits to use."})
+    lora_modules: Optional[List[str]] = field(
+        default=None,
+        metadata={
+            "help": "Which modules to use LoRA on. If None, will use all linear layers."
+        },
+    )
+    lora_r: int = field(default=64, metadata={"help": "Lora R dimension."})
+    lora_alpha: float = field(default=16, metadata={"help": " Lora alpha."})
+    lora_dropout: float = field(default=0.0, metadata={"help": "Lora dropout."})
+    report_to: str = field(
+        default="none",
+        metadata={"help": "To use wandb or something else for reporting."},
+    )
+    resume_dir: Optional[str] = field(
+        default=None,
+        metadata={"help": "Path to the directory containing the checkpoint to resume."},
+    )
+    output_dir: str = field(
+        default="./output", metadata={"help": "The output dir for logs and checkpoints"}
+    )
+    optim: str = field(
+        default="paged_adamw_32bit", metadata={"help": "The optimizer to be used"}
+    )
+    per_device_train_batch_size: int = field(
+        default=1,
+        metadata={
+            "help": "The training batch size per GPU. Increase for better speed."
+        },
+    )
+    gradient_accumulation_steps: int = field(
+        default=16,
+        metadata={
+            "help": "How many gradients to accumulate before to perform an optimizer step"
+        },
+    )
+    weight_decay: float = field(
+        default=0.0, metadata={"help": "The L2 weight decay rate of AdamW"}
+    )  # use lora dropout instead for regularization if needed
+    learning_rate: float = field(default=0.0002, metadata={"help": "The learnign rate"})
+    remove_unused_columns: bool = field(
+        default=False,
+        metadata={"help": "Removed unused columns. Needed to make this codebase work."},
+    )
+    max_grad_norm: float = field(
+        default=0.3,
+        metadata={
+            "help": "Gradient clipping max norm. This is tuned and works well for all models tested."
+        },
+    )
+    gradient_checkpointing: bool = field(
+        default=True,
+        metadata={"help": "Use gradient checkpointing. You want to use this."},
+    )
+    do_train: bool = field(
+        default=True,
+        metadata={"help": "To train or not to train, that is the question?"},
+    )
+    lr_scheduler_type: str = field(
+        default="constant",
+        metadata={
+            "help": "Learning rate schedule. Constant a bit better than cosine, and has advantage for analysis"
+        },
+    )
+    warmup_ratio: float = field(
+        default=0.03, metadata={"help": "Fraction of steps to do a warmup for"}
+    )
+    logging_steps: int = field(
+        default=10,
+        metadata={"help": "The frequency of update steps after which to log the loss"},
+    )
+    group_by_length: bool = field(
+        default=True,
+        metadata={
+            "help": "Group sequences into batches with same length. Saves memory and speeds up training considerably."
+        },
+    )
+    save_strategy: str = field(
+        default="steps", metadata={"help": "When to save checkpoints"}
+    )
+    save_steps: int = field(default=250, metadata={"help": "How often to save a model"})
+    save_total_limit: int = field(
+        default=40,
+        metadata={
+            "help": "How many checkpoints to save before the oldest is overwritten"
+        },
+    )
+    resume_from_training: bool = field(
+        default=False, metadata={"help": "Resume from training"}
+    )
+
+
+def main():
+    hfparser = transformers.HfArgumentParser(
+        (ModelArguments, DataArguments, TrainingArguments)
+    )
+    (
+        model_args,
+        data_args,
+        training_args,
+        extra_args,
+    ) = hfparser.parse_args_into_dataclasses(return_remaining_strings=True)
+
+    bits = 4
+    bits_and_bytes_config = BitsAndBytesConfig(
+        load_in_4bit=bits == 4,
+        load_in_8bit=bits == 8,
+        llm_int8_threshold=6.0,
+        llm_int8_has_fp16_weight=False,
+        bnb_4bit_compute_dtype=torch.bfloat16,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+        llm_int8_skip_modules=["mm_projector", "lm_head"],
+    )
+
+    model_id = "llava-hf/bakLlava-v1-hf"
+    current_device = torch.cuda.current_device()
+    model = LlavaForConditionalGeneration.from_pretrained(
+        model_id,
+        low_cpu_mem_usage=True,
+        load_in_4bit=bits == 4,
+        load_in_8bit=bits == 8,
+        device_map={"": current_device},
+        quantization_config=bits_and_bytes_config,
+        torch_dtype=torch.bfloat16,
+        trust_remote_code=False,
+    )
+    processor = AutoProcessor.from_pretrained(model_id)
+
+    dataset = load_dataset("zhiqings/LLaVA-Human-Preference-10K")
+    peft_config = LoraConfig(
+        r=16,
+        lora_alpha=16,
+        bias="none",
+        task_type="SEQ_CLS",
+        modules_to_save=["scores"],
+    )
+
+    trainer = RewardTrainer(
+        model=model,
+        tokenizer=processor,
+        args=training_args,
+        train_dataset=dataset["train"],
+        # eval_dataset=eval_dataset,
+        peft_config=peft_config,
+    )
+    trainer.train()
+    trainer.save_model(training_args.output_dir)
+
+
+if __name__ == "__main__":
+    main()
