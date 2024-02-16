48b15df614bc15a4bf1cf07163f5bc580e49812d
Fraser Mince <frasermince@gmail.com>
2024-02-16 13:38:32 -0600

    Add multi device setup.

--- bakllava_rlhf/train_reward_model.sh
+++ bakllava_rlhf/train_reward_model.sh
@@ -1,4 +1,8 @@
 export HF_DATASETS_CACHE="/workspace/.cache/huggingface/datasets"
 export HF_HOME="/workspace/.cache/huggingface/misc"
 export TRANSFORMERS_CACHE="/workspace/.cache/huggingface/transformers"
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export GPUS_PER_NODE=8
+export OMP_NUM_THREADS=8
+
 poetry run python -m bakllava_rlhf.train_reward_model
