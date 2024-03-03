b8a5127c283d3500b48ce9f7b60fe661f68b0582
Fraser Mince <frasermince@gmail.com>
2024-03-03 13:55:05 -0600

    add possible configs to fix variable marked twice

--- bakllava_rlhf/train_reward_model.sh
+++ bakllava_rlhf/train_reward_model.sh
@@ -3,9 +3,10 @@ export HF_DATASETS_CACHE="${HUGGINGFACE_CACHE}/datasets"
 export HF_HOME="${HUGGINGFACE_CACHE}/misc"
 export TRANSFORMERS_CACHE="${HUGGINGFACE_CACHE}/transformers"
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
-export GPUS_PER_NODE=8
-export OMP_NUM_THREADS=8
+export CUDA_VISIBLE_DEVICES=0#,1,2,3,4,5,6,7
+export GPUS_PER_NODE=1
+export OMP_NUM_THREADS=1
+export TORCH_DISTRIBUTED_DEBUG=INFO
 
 poetry run torchrun \
     --standalone \
@@ -23,6 +24,7 @@ poetry run torchrun \
     --ddp_find_unused_parameters False \
     --query_len 1280 \
     --response_len 768 \
+    --lora_modules q_proj k_proj v_proj o_proj gate_proj up_proj down_proj \
     --reward_prompt_file "./prompts/fact_rlhf_reward_prompt.txt" \
     --image_to_caption_file "$DATA_DIR/image_to_caption.json" \
     --image_aspect_ratio 'pad'
